b356acd00d1aa503d9ea03c71c337a8a5babb214
Kris Jenkins <krisajenkins@gmail.com>
2025-03-20 14:40:58 +0000

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use vfs::{MemoryFS, VfsPath, VfsResult};
+
+    #[test]
+    fn dumb_test() -> VfsResult<()> {
+        let root: VfsPath = MemoryFS::new().into();
+        let path = root.join("somefile.txt")?;
+        if true {
+            let mut file = path.create_file()?;
+            file.write_all(b"Hello world\n")?;
+        }
+
+        let mut buf = String::new();
+        path.open_file()?.read_to_string(&mut buf)?;
+        assert_eq!("Hello world\n", buf);
+
+        Ok(())
+    }
+}
